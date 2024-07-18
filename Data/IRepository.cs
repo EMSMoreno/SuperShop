986371d9447c7dd878627267b7316a3dc14f59b7
Eduardo M <emsmoreno97@gmail.com>
2024-07-18 16:16:42 +0100

    Repository

---
+++ Data/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public interface IRepository
+    {
+        void AddProduct(Product product);
+
+
+        void DeleteProduct(Product product);
+
+
+        Product GetProduct(int id);
+
+
+        IEnumerable<Product> GetProducts();
+
+
+        bool ProductExists(int id);
+
+
+        Task<bool> SaveAllAsync();
+
+
+        void UpdateProduct(Product product);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>();
+            services.AddScoped<IRepository, Repository>();
 
             services.AddControllersWithViews();
         }
