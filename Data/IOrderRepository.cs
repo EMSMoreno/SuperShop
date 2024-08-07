c9fa31a2f4880c6fceb08e0298464ff7309846a7
EMSMoreno <emsmoreno97@gmail.pt>
2024-08-07 13:04:43 +0100

    Products Combo

--- Data/IOrderRepository.cs
+++ Data/IOrderRepository.cs
@@ -7,5 +7,7 @@ namespace SuperShop.Data
     public interface IOrderRepository : IGenericRepository<Order>
     {
         Task<IQueryable<Order>> GetOrderAsync(string username);
+
+        Task<IQueryable<OrderDetailTemp>> GetDetailTempsAsync(string username);
     }
 }
--- Data/ProductRepository.cs
+++ Data/ProductRepository.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SuperShop.Data
@@ -8,14 +10,31 @@ namespace SuperShop.Data
     {
         private readonly DataContext _context;
 
-        public ProductRepository(DataContext context) : base(context) 
+        public ProductRepository(DataContext context) : base(context)
         {
             _context = context;
         }
 
-        public IQueryable GetAllWithUsers ()
+        public IQueryable GetAllWithUsers()
         {
             return _context.Products.Include(p => p.User); //Como se fosse um innerjoin entre as duas tabelas, dá-me os produtos com os utilizadores
+        }
+
+        public IEnumerable<SelectListItem> GetComboProducts()
+        {
+            var list = _context.Products.Select(p => new SelectListItem
+            {
+                Text = p.Name,
+                Value = p.Id.ToString(),
+            }).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a product...)",
+                Value = "0"
+            });
+
+            return list;
         }
     }
 }
